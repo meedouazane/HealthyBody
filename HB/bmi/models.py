bd659d1f4bb0554d9d37061f8b3de59887b2bb07
Meed <mohamedouazane@yahoo.com>
2024-07-28 14:46:05 +0100

    add comments and rename date field

--- HB/bmi/models.py
+++ HB/bmi/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import AbstractUser
 import uuid
 from django.conf import settings
 
 
 class CustomUser(AbstractUser):
+    """ Customize user model """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sex = models.CharField(null=True, max_length=100)
     date_of_birth = models.DateField(null=True)
@@ -16,14 +17,10 @@ class CustomUser(AbstractUser):
 
 
 class Records(models.Model):
-    """ User table """
+    """ table for bmi """
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     weight = models.FloatField()
     height = models.FloatField()
-    data = models.DateField(default=timezone.now)
+    date = models.DateField(default=timezone.now)
     bmi = models.FloatField()
-
-    def __str__(self):
-        """ return string object"""
-        return self.user_id
