a065301cb98e4529ed0f52aab5b76d5c70965e7c
Meed <mohamedouazane@yahoo.com>
2024-07-29 16:09:33 +0100

    rename route to set_bmi

--- HB/bmi/urls.py
+++ HB/bmi/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', views_auth.login),
     path('signup/', views_auth.signup),
     path('user/', views.get_user),
-    path('create_bmi/', views.create_bmi),
+    path('set_bmi/', views.create_bmi),
     path('get_bmi/', views.get_bmi),
     path('delete/', views.user_delete),
     path('update/', views.user_update),
